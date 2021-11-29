394171fe8eae9bcb35fac2a9fb043a6db85c573c
MejdHub <88592384+MejdHub@users.noreply.github.com>
2021-11-29 09:35:08 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 25 18:30:13 2021
+
+@author: MA-PC
+"""
+import numpy   as np
+import random  as rd
+import pygame  as pg
+import neural  as ai
+import sys
+import dataset as ds
+import pandas  as ps
+
+
+if __name__ == "__main__":
+    #nn = ai.NeuralNetwork(rIn,gIn,bIn,z)
+    
+    
