bb026dd8e8af3b69f9565b01e4de5e59f3bd8020
MejdHub <88592384+MejdHub@users.noreply.github.com>
2021-11-29 09:40:14 +0300

    Create main.py

---
+++ main/main.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 25 18:30:13 2021
+
+@author: Am-pc
+"""
+
+import numpy   as np
+import random  as rd
+import pygame  as pg
+import neural  as ai
+import sys
+import dataset as ds
+import pandas  as ps
+
+
+if __name__ == "__main__":
+    
+    #nn = ai.NeuralNetwork(rIn,gIn,bIn,z)
+    
+    
+    nn.train(1)
+    
+    a = np.array([[0.000]])
+    b = np.array([[0.000]])
+    c = np.array([[0.000]])
+    
+    print(nn.outputprod(a, b, c))
+    print("\r\n")
+    print(nn.prob)
+    print("\r\n\ r\n\ r\n")
+    print(nn.weights1)
+    print(nn.weights2)
+    print(nn.weights3)
+    print(nn.weights4)
+    print(nn.weights5)
+    print(nn.weights6)
+    print(nn.weights7)
+    print(nn.weights8)
+    print(nn.weights9)
+    print(nn.weights10)
+    print(nn.weights11)
+    print(nn.weights12)
+    print(nn.weights13)
+    print(nn.weights14)
+    print(nn.weights15)
+    print(nn.weights16)
+    print(nn.weights17)
+    '''
